ea0428f347781df02236ad08bf02b5f1dd47f5da
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-12-03 22:41:52 -0500

    Update all files

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
+//Ask users to choose color & 
 const sectionButtons = document.querySelectorAll('section');
 const section = document.querySelector('p');
 
 function wrapper() {
 
   const changColor = () => {
-  	section.classList.toggle('black');
+  	section.classList.toggle('red');
   };
 
   const loopThrough = () => {
   	let thisButtonText = this.childNodes[0].nodeValue;
   	let thisButtonLength = thisButtonText.length;
 
-    for (let i = 0; i < thisButtonLength * 2; i++) {
+    for (let i = 0; i < thisButtonLength; i++) {
     	setTimeout(function() {
     		changColor();
     	} , i * 1000);
@@ -23,3 +24,5 @@ function wrapper() {
 for (let i = 0; i < sectionButtons.length; i++) {
 	sectionButtons[i].addEventListener('click', wrapper);
 };
+
+
