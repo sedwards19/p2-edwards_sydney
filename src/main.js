46025084093ad4d853460b0ac48bdefd0a501d5c
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-12-09 10:00:49 -0500

    Update all files with styling and javascript if else

--- src/main.js
+++ src/main.js
@@ -1,24 +1,37 @@
 //Ask users to choose color & 
 const sectionButtons = document.querySelectorAll('section');
-const section = document.querySelector('p');
+const section = document.getElementsByTagName('p');
+
+const firstFunctionIndexNumbers = [0,1,4,5,8,9,12,13,16,17,20,21,,24,25];
+
+const changColor = () => {
+  section.classList.toggle('red'); //replace line with loop
+};
+
+const changColor2 = () => {
+  section.classList.toggle('blue'); //replace line with loop
+};
 
 function wrapper() {
-
-  const changColor = () => {
-  	section.classList.toggle('red');
-  };
 
   const loopThrough = () => {
   	let thisButtonText = this.childNodes[0].nodeValue;
   	let thisButtonLength = thisButtonText.length;
 
-    for (let i = 0; i < thisButtonLength; i++) {
+    for (let i = 0; i < thisButtonLength * 2; i++) {
     	setTimeout(function() {
-    		changColor();
-    	} , i * 1000);
+    		if (firstFunctionIndexNumbers.includes(i)) {
+          changColor();
+        } else {
+          changColor2();
+        }
+      }, i *500);
     }
+
   };
   loopThrough();
+
+
 };
 
 for (let i = 0; i < sectionButtons.length; i++) {
